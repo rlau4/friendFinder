62e7b92517728c560056cd483730cd99f24f3b54
rlau4 <p.lau.004@gmail.com>
2019-06-18 11:09:47 -0400

    updated pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friends = [
     {
         name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.birmingham.ac.uk%2FImages%2FStaff%2Fprofiles%2Fcem%2FAhmed%2CZubair.jpg&imgrefurl=https%3A%2F%2Fwww.birmingham.ac.uk%2Fstaff%2Fprofiles%2Finflammation-ageing%2FAhmed-Zubair.aspx&docid=fuhLCzjFJzorHM&tbnid=T5pDKKDgll07DM%3A&vet=10ahUKEwiD5P7Mp_PiAhVFI6wKHdWXBMQQMwh-KAIwAg..i&w=230&h=230&bih=920&biw=1920&q=ahmed&ved=0ahUKEwiD5P7Mp_PiAhVFI6wKHdWXBMQQMwh-KAIwAg&iact=mrc&uact=8",
         scores : [
             5,1,4,4,5,1,2,5,4,1
         ]
@@ -23,14 +23,14 @@ var friends = [
     },
     {
         name: "Louis",
-        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.nola.com%2Fhome%2Fnola-media%2Fwidth600%2Fimg%2Fentertainment_impact_music%2Fphoto%2F1959-press-photo-louis-armstrong-louisiana-jazz-trumpeter-ae291c86a28d3062.jpg&imgrefurl=https%3A%2F%2Fwww.nola.com%2Fmusic%2F2018%2F09%2Farmstrong_marijuana_history_ch.html&docid=ZqguHtGApjo-DM&tbnid=9Y8wDjbG8VEDFM%3A&vet=10ahUKEwjI8KDXp_PiAhVFeKwKHWYCD2kQMwh9KAEwAQ..i&w=600&h=497&bih=920&biw=1920&q=louis%20armstrong&ved=0ahUKEwjI8KDXp_PiAhVFeKwKHWYCD2kQMwh9KAEwAQ&iact=mrc&uact=8",
         scores: [
             3,3,4,2,2,1,3,2,2,3
         ]
     },
     {
         name: "Lou",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
         scores: [
             4,3,4,1,5,2,5,3,1,4
         ]
@@ -43,8 +43,8 @@ var friends = [
         ]
     },
     {
-        name: "Jordan",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        name: "Mike",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.nba.net%2Fnba-drupal-prod%2Fstyles%2Flandscape%2Fhttp%2Fi2.cdn.turner.com%2Fnba%2Fnba%2Fdam%2Fassets%2F130828123801-michael-jordan-iso-1998-all-star-game.video-player.jpg%3Fitok%3D6H7vzj5I&imgrefurl=http%3A%2F%2Fwww.nba.com%2Fhistory%2Flegends%2Fprofiles%2Fmichael-jordan&docid=CzVP1tIcQlzVoM&tbnid=WUJDhch4fO6vUM%3A&vet=10ahUKEwiJm9Trp_PiAhUKT6wKHediBesQMwh7KAAwAA..i&w=768&h=432&bih=920&biw=1920&q=michael%20jordan&ved=0ahUKEwiJm9Trp_PiAhUKT6wKHediBesQMwh7KAAwAA&iact=mrc&uact=8https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
         scores: [
             4,4,2,3,2,2,3,2,4,5
         ]
